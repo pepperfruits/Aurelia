81f5632199396fcd68fd8ac4956e44da62f753ec
pepperfruits <fruitskitty54@gmail.com>
2024-07-17 14:43:51 -0400

    Jump is now bufferable

--- Player/InputHandler.gd
+++ Player/InputHandler.gd
@@ -1,8 +1,20 @@
 extends Node
 class_name InputHandler
 
+@onready var JumpBufferTimer = $JumpBufferTimer
+
+var is_jump_buffered : bool = false
+
+func _process(delta):
+	if Input.is_action_just_pressed("jump"):
+		is_jump_buffered = true
+		JumpBufferTimer.start()
+
 func get_horizontal_input() -> float:
 	return Input.get_axis("move_left", "move_right")
 
 func is_jump_inputted() -> bool:
-	return Input.is_action_just_pressed("jump")
+	return is_jump_buffered
+
+func _on_jump_buffer_timer_timeout():
+	is_jump_buffered = false
