03cc3a1c21546cc7383dc9e88bf4df1d7daf4f5a
pepperfruits <fruitskitty54@gmail.com>
2024-07-17 14:10:07 -0400

    Movement tweaks & comments

--- Player/Movement.gd
+++ Player/Movement.gd
@@ -1,14 +1,16 @@
 extends Node
 
 #region Nodes
-@onready var p : CharacterBody2D = $".."
+## For ease of programming, the player is p
+@onready var p : CharacterBody2D = $".." 
 #endregion
 
 #region Constants
 @export var RUN_ACCEL : float = 800.0
 @export var AIR_ACCEL_BONUS : float = 0.75
 @export var RUN_MAX_SPEED : float = 400.0
-@export var FRICTION : float = 5.0
+@export var FRICTION : float = 8.0
+@export var AIR_FRICTION_BONUS : float = 0.75
 #endregion
 
 #region Variables
@@ -19,16 +21,17 @@ func _process(delta):
 	var is_on_floor : bool = p.is_on_floor()
 	var input_direction : float = Input.get_axis("move_left", "move_right")
 	
+	# Movement Acc
 	if (input_direction):
 		apply_acceleration(delta, input_direction, is_on_floor)
 	else:
 		apply_friction(delta, is_on_floor)
 	
-	$"../DebugLabel".text = str(momentum)
+	$"../DebugLabel".text = str(momentum) # TODO debug, delete
 	p.velocity.x = momentum
 	p.move_and_slide()
 
-func cap_momentum(delta : float) -> void:
+func cap_momentum(_delta : float) -> void:
 	momentum = min(momentum, RUN_MAX_SPEED)
 	momentum = max(momentum, -RUN_MAX_SPEED)
 
@@ -42,8 +45,11 @@ func apply_acceleration(delta : float, input_direction : float, is_on_floor : bo
 
 func apply_friction(delta : float, is_on_floor : bool) -> void:
 	if (momentum != 0.0):
-		momentum -= momentum * delta * FRICTION
+		if (is_on_floor):
+			momentum -= momentum * delta * FRICTION
+		else:
+			momentum -= momentum * delta * FRICTION * AIR_FRICTION_BONUS
 		
-		var movement_kill_zone : float = 1.0
+		var movement_kill_zone : float = 4.0
 		if (momentum < movement_kill_zone and momentum > -movement_kill_zone):
 			momentum = 0.0
