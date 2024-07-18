bcc70d2ca53f7e3e2255a783dacdd5b933b49fe6
pepperfruits <fruitskitty54@gmail.com>
2024-07-18 19:12:11 -0400

    Health, hitboxes, hurtboxes, spikes, level transition, window changes

---
+++ Scripts/Hitbox.gd
@@ -0,0 +1,7 @@
+extends Area2D
+class_name Hitbox
+
+@export var damage : int = 1
+@export var knockback : float = 1200.0
+@export var is_custom_knockback_direction : bool = false
+@export var custom_knockback_direction : Vector2 = Vector2.UP
