681ce5353aaea6e87ae8f0ebcc6864921211dddc
pepperfruits <fruitskitty54@gmail.com>
2024-07-18 16:20:18 -0400

    Hook aura no long shows up when disabled

--- Environment/Hook/hook.gd
+++ Environment/Hook/hook.gd
@@ -6,6 +6,7 @@ class_name HookArea2D
 @onready var collision : CollisionShape2D = $HookAreaCollisionShape
 @onready var sprite : Sprite2D = $HookSprite
 @onready var spriteAura : Sprite2D = $HookAuraSprite
+@onready var auraRange : Area2D = $AuraRange
 @export var spriteEnabled : Texture
 @export var spriteDisabled : Texture
 @export var AURA_ROTATION_SPEED : float = 360.0
@@ -28,14 +29,17 @@ func use():
 	collision.disabled = true
 	p._on_player_exited_hook_range(self)
 	sprite.texture = spriteDisabled
+	spriteAura.visible = false
 
 func _on_hook_cooldown_timer_timeout():
 	collision.disabled = false
 	sprite.texture = spriteEnabled
-
+	if not auraRange.get_overlapping_bodies().is_empty():
+		spriteAura.visible = true
 
 func _on_aura_range_body_entered(body):
-	spriteAura.visible = true
+	if not collision.disabled:
+		spriteAura.visible = true
 
 func _on_aura_range_body_exited(body):
 	spriteAura.visible = false
