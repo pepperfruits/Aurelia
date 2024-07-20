5d2f9a4a55bd7c167e50041c36bce0a4365900bc
pepperfruits <fruitskitty54@gmail.com>
2024-07-20 02:51:57 -0400

    platform cleanup

--- Environment/Platform/platform.gd
+++ Environment/Platform/platform.gd
@@ -5,8 +5,15 @@ class_name Platform
 
 @onready var p : PlayerCharacter = $"..".p
 @onready var CollisionShape : CollisionShape2D = $PlatformCollisionShape
-@onready var PlayerDetection : Area2D = $PlayerDetectionArea
+@onready var DisablePlatformDetection : Area2D = $DisablePlatformDetection
 
 func _process(_delta):
-	var inp : PlayerInputHandler = p.InputHandler
-	CollisionShape.disabled = inp.is_down_inputted() or (inp.get_vertical_input() < 0 and p.velocity.y < HOLD_TO_FALL_VELOCITY) or p.velocity.y < 0 or not PlayerDetection.get_overlapping_bodies().is_empty()
+	CollisionShape.disabled = is_disabled()
+
+func is_disabled():
+	var down_pressed : bool = p.InputHandler.is_down_inputted()
+	var down_held : bool = (p.InputHandler.get_vertical_input() < 0 and p.velocity.y < HOLD_TO_FALL_VELOCITY)
+	var going_up : bool = p.velocity.y < 0
+	var inside_platform : bool = not DisablePlatformDetection.get_overlapping_bodies().is_empty()
+	
+	return down_pressed or down_held or going_up or inside_platform
