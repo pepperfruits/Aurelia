5c9981a1344da2a86ffd368e22760f39e473bc03
pepperfruits <fruitskitty54@gmail.com>
2024-07-21 02:21:20 -0400

    fixed air draft toggle glitch

--- Environment/AirDraft/AirDraft.gd
+++ Environment/AirDraft/AirDraft.gd
@@ -5,24 +5,17 @@ class_name AirDraft
 @export var FORCE : float = 2550.0
 @export var STOPPING_BONUS : float = 2.0
 
-var p : PlayerCharacter = null
-
 # NOTICE dont try to make this rotated anywhere but up for now
 
 func _process(delta):
-	if p:
+	if not get_overlapping_bodies().is_empty():
+		var p : PlayerCharacter = get_overlapping_bodies().front()
 		p.refresh_dash_charges()
 		if p.velocity.y > -MAX_SPEED:
-			apply_force(delta)
+			apply_force(delta, p)
 
-func apply_force(delta) -> void:
+func apply_force(delta : float, p : PlayerCharacter) -> void:
 	if p.velocity.y > 0:
 		p.set_player_velocity(Vector2(p.velocity.x, p.velocity.y) + Vector2(0, -FORCE) * delta * STOPPING_BONUS * max(1,(abs(p.velocity.y) / MAX_SPEED)))
 	else:
 		p.set_player_velocity(Vector2(p.velocity.x, p.velocity.y) + Vector2(0, -FORCE) * delta)
-
-func _on_body_entered(body):
-	p = body
-
-func _on_body_exited(_body):
-	p = null
