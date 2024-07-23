fb3389302f54fda675587b23aef4e783be7e8429
jppepper <fruitskitty54@gmail.com>
2024-07-23 16:54:52 -0400

    coins done
    moves much faster, collected between levels and stored like death #.

--- Zones/LevelChangeArea/LevelChangeArea.gd
+++ Zones/LevelChangeArea/LevelChangeArea.gd
@@ -1,6 +1,8 @@
 extends Area2D
+class_name LevelChangeArea
 
 @export var scene : PackedScene
+@export var coinArea : CoinCollectionArea
 
 @onready var timer : Timer = $LevelChangeTimer
 
@@ -9,4 +11,5 @@ func _on_body_entered(body : PlayerCharacter):
 	body.set_camera_transition(true)
 
 func _on_level_change_timer_timeout():
+	ScoreManager.coins += coinArea.get_overlapping_areas().size()
 	get_tree().change_scene_to_packed(scene)
