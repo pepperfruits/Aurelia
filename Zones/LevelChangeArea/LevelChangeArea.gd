3a6d42edf8e27c086d64be5b11a400b1b594e2d5
pepperfruits <fruitskitty54@gmail.com>
2024-07-27 01:48:53 -0400

    coin ID
    to prevent previously collected coins from respawning

--- Zones/LevelChangeArea/LevelChangeArea.gd
+++ Zones/LevelChangeArea/LevelChangeArea.gd
@@ -12,4 +12,6 @@ func _on_body_entered(body : PlayerCharacter):
 
 func _on_level_change_timer_timeout():
 	ScoreManager.coins += coinArea.get_overlapping_areas().size()
+	for i : Coin in coinArea.get_overlapping_areas():
+		ScoreManager.coinArray.append(i.COIN_ID)
 	get_tree().change_scene_to_packed(scene)
