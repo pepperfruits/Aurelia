c42033726a56347a652e7f54192378fd7125d637
pepperfruits <fruitskitty54@gmail.com>
2024-07-28 03:33:48 -0400

    bullet particles, bullet is faster

--- Projectiles/PlayerBullet/player_bullet.gd
+++ Projectiles/PlayerBullet/player_bullet.gd
@@ -2,6 +2,8 @@ extends Hitbox
 class_name Projectile
 
 @export var velocity : Vector2 = Vector2(3000.0,0)
+@export var acceleration : Vector2 = Vector2(1000.0,0)
+@export var bulletParticles : PackedScene
 @onready var Sprite : Sprite2D = $Sprite
 @onready var Collision : CollisionShape2D = $CollisionShape2D
 @onready var Particles : GPUParticles2D = $GPUParticles2D
@@ -12,12 +14,24 @@ func _ready():
 		Sprite.scale.x *= -1
 		Collision.position.x *= -1
 		Particles.position.x *= -1
+		acceleration.x *= -1
 
 func _process(delta):
+	if velocity.length() >= 3000:
+		velocity += acceleration * 0.5 * delta
 	position += velocity * delta
+	if velocity.length() >= 3000:
+		velocity += acceleration * 0.5 * delta
 
 func _on_body_entered(_body):
+	particles()
 	self.queue_free()
 
 func _on_area_entered(_area):
+	particles()
 	self.queue_free()
+
+func particles():
+	var part = bulletParticles.instantiate()
+	get_tree().current_scene.add_child(part)
+	part.global_position = global_position
