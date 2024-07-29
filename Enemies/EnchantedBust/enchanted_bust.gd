da8a22746a221f848bd41804fea5de9f7abcd2cb
pepperfruits <fruitskitty54@gmail.com>
2024-07-29 03:20:41 -0400

    fully working enchanted bust

---
+++ Enemies/EnchantedBust/enchanted_bust.gd
@@ -0,0 +1,23 @@
+extends StaticBody2D
+class_name EnchantedBust
+
+@onready var Sprite : Sprite2D = $Sprite
+@onready var Light : PointLight2D = $Light
+
+@export var EYES_CLOSED : Texture2D
+@export var EYES_OPEN : Texture2D
+@export var Bullet : PackedScene
+@export var ShootingMarker : Marker2D
+
+func shoot() -> void:
+	var bullet = Bullet.instantiate()
+	bullet.global_position = ShootingMarker.global_position
+	get_tree().current_scene.add_child(bullet)
+
+func close_eyes() -> void:
+	Sprite.texture = EYES_CLOSED
+	Light.enabled = false
+
+func open_eyes() -> void:
+	Sprite.texture = EYES_OPEN
+	Light.enabled = true
