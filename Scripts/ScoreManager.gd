2a6056bdf1298935a55ac3cab5a5334a8a527249
pepperfruits <fruitskitty54@gmail.com>
2024-07-30 01:29:46 -0400

    pause menu quit resets more things

--- Scripts/ScoreManager.gd
+++ Scripts/ScoreManager.gd
@@ -9,3 +9,11 @@ var particles_enabled : bool = true
 var control_scheme : int = 0
 
 var coinArray : Array[int] = []
+
+func restart():
+	deaths = 0
+	coins = 0
+	time = 0.0
+	particles_enabled = true
+	control_scheme = 0
+	coinArray.clear()
