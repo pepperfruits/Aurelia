c1f59f41f99bc102ae5907183b516239536e0803
pepperfruits <fruitskitty54@gmail.com>
2024-07-30 20:42:27 -0400

    main menu sfx

--- Audio/MusicPlayer/music_player.gd
+++ Audio/MusicPlayer/music_player.gd
@@ -1,9 +1,14 @@
 extends Node
 
 var level1_music = load("res://Audio/Music/BGM.wav")
+var mainmenu_music = load("res://Audio/Music/mainmenu.wav")
 
 func _ready():
 	pass
+
+func play_menu_music():
+	$Music.stream = mainmenu_music
+	$Music.play()
 
 func play_music():
 	$Music.stream = level1_music
