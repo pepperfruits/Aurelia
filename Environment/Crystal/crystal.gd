0eb304e68d3d43d95cd6156af9cfbd640d147235
pepperfruits <fruitskitty54@gmail.com>
2024-07-30 21:30:41 -0400

    crystal sfx

--- Environment/Crystal/crystal.gd
+++ Environment/Crystal/crystal.gd
@@ -8,6 +8,10 @@ var p : PlayerCharacter = null
 ## particles that you make when you teleport
 @export var TeleportParticles : PackedScene = preload("res://Particles/TeleportParticles/teleport_particles.tscn")
 
+@onready var sfx : PackedScene = preload("res://Audio/sfx_player.tscn")
+@export var sound1 : AudioStream
+@export var sound2 : AudioStream
+
 func _process(_delta):
 	if p:
 		var h = p.InputHandler.get_horizontal_input()
@@ -34,6 +38,10 @@ func _on_area_entered(bullet : Projectile):
 func teleport_player():
 	p.global_position = global_position
 	p.set_player_velocity(Vector2(0,0))
+	var s : AudioStreamPlayer = sfx.instantiate()
+	s.stream = sound1
+	s.volume_db = 3
+	get_tree().current_scene.add_child(s)
 
 func set_pointer(b : bool) -> void:
 	PointerSprite.visible = b
@@ -41,6 +49,11 @@ func set_pointer(b : bool) -> void:
 func use():
 	set_pointer(false)
 	p = null
+	
+	var s : AudioStreamPlayer = sfx.instantiate()
+	s.stream = sound2
+	s.volume_db = 3
+	get_tree().current_scene.add_child(s)
 
 
 func _on_flashing_timer_timeout():
