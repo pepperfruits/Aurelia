419836e67ad5d2f80b13bc12f7166940978d995e
pepperfruits <fruitskitty54@gmail.com>
2024-07-30 22:02:19 -0400

    rng dash sfx

---
+++ Player/PlayerAudio.gd
@@ -0,0 +1,21 @@
+extends Node
+class_name PlayerAudio
+
+@onready var sfx : PackedScene = preload("res://Audio/sfx_player.tscn")
+@onready var rng : RandomNumberGenerator = RandomNumberGenerator.new()
+
+@export var dash_sfx_1 : AudioStream
+@export var dash_sfx_2 : AudioStream
+
+func play(sound : AudioStream, db : float):
+	var s : AudioStreamPlayer = sfx.instantiate()
+	s.stream = sound
+	s.volume_db = db
+	add_child(s)
+
+func dash():
+	match rng.randi_range(0,1):
+		0:
+			play(dash_sfx_1, 0)
+		1:
+			play(dash_sfx_2, 0)
