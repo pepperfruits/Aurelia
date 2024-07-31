89e89ff367b1eb5913996a21179e77851ab8cc1a
pepperfruits <fruitskitty54@gmail.com>
2024-07-31 00:18:05 -0400

    1-6 less tedious

--- Environment/Managers/PathFollowManager/path_follow_manager.gd
+++ Environment/Managers/PathFollowManager/path_follow_manager.gd
@@ -1,7 +1,8 @@
 extends PathFollow2D
 
 @export var swing : bool = true
+@export var offset : float = 0.0
 @onready var cycle : CycleManager = $"../.."
 
 func _process(_delta : float):
-	progress_ratio = cycle.time
+	progress_ratio = cycle.time + offset
